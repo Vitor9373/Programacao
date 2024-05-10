564d796c4db2fc452c0c1e395eab38094ae2621b
Vitor9373 <131017343+Vitor9373@users.noreply.github.com>
2024-05-10 13:51:42 +0000

    1 e 2 100%

--- Aula1/e1.cpp
+++ Aula1/e1.cpp
@@ -6,12 +6,16 @@ class Circulo{
         Circulo(){};
         ~Circulo(){};
 
-        float calcArea(float raio){
+        float calcArea(float raio) const{
             return pi * (raio * raio);
         }
 
-        float calcPeri(float raio){
+        float calcPeri(float raio) const{
             return 2 * pi * raio;
+        }
+
+        void set(float _raio){
+            raio = _raio;
         }
     
     private:
@@ -25,6 +29,7 @@ int main(){
     std::cin >> raio;
     area = circulo.calcArea(raio);
     perimetro = circulo.calcPeri(raio);
+    circulo.set(raio);
     std::cout << "\n\nA área é " << area;
     std::cout << "\nO perímetro é " << perimetro;
     return 0;
