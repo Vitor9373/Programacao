ddd83aa0a20f912ad36990375d432a079c1cf1c5
Vitor9373 <131017343+Vitor9373@users.noreply.github.com>
2024-05-15 01:25:39 +0000

    Começada questão 5

--- Aula1/e5.cpp
+++ Aula1/e5.cpp
@@ -1,3 +1,26 @@
 //Crie uma classe chamada “Funcionário” com atributos para armazenar o nome, o
 //salário e o cargo do funcionário. Implemente métodos para calcular o salário líquido,
 //considerando descontos de impostos e benefícios.
+#include <iostream>
+
+class Funcionario{
+    public:
+        Funcionario(){};
+        ~Funcionario(){};
+
+    private:
+        std::string nome, cargo;
+        float salario;
+};
+
+int main(){
+    std::string nome, cargo;
+    float salario;
+    std::cout << "Digite seu nome: ";
+    std::cin >> nome;
+    std::cout << "Digite seu cargo: ";
+    std::cin >> cargo;
+    std::cout << "Digite seu salário: ";
+    std::cin >> salario;
+    
+}
