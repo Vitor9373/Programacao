ebf7b423d833368468a071c7231c696a66812f9e
Vitor9373 <131017343+Vitor9373@users.noreply.github.com>
2024-05-15 00:08:06 +0000

    Questão 5 tá aí

---
+++ Aula1/e5.cpp
@@ -0,0 +1,3 @@
+//Crie uma classe chamada “Funcionário” com atributos para armazenar o nome, o
+//salário e o cargo do funcionário. Implemente métodos para calcular o salário líquido,
+//considerando descontos de impostos e benefícios.
